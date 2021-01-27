0fdd9252f50c1adf9bcc8eb6e4e5dd9e190309b6
noamhoresh <59334606+noamhoresh@users.noreply.github.com>
2021-01-27 09:02:35 +0200

    Update client.py
    main()
    logout()
    error_exit()

--- client.py
+++ client.py
@@ -4,17 +4,26 @@ from chatlib_skeleton import *  # To use chatlib functions or consts, use chatli
 SERVER_IP = "127.0.0.1"  # Our server will run on same computer as client
 SERVER_PORT = 5678
 
-# HELPER SOCKET METHODS
+"""
+Builds a new message using chatlib, wanted code and message. 
+Prints debug info, then sends it to the given socket.
+Paramaters: conn (socket object), code (str), msg ( .str)
+Returns: Nothing
+"""
+
 
 def build_and_send_message(conn, code, msg):
-	"""
-	Builds a new message using chatlib, wanted code and message. 
-	Prints debug info, then sends it to the given socket.
-	Paramaters: conn (socket object), code (str), msg ( .str)
-	Returns: Nothing
-	"""
-	message = build_message(code, msg)# genretaing the message
-    conn.send(message.encode())# send the message
+    message = build_message(code, msg)
+    conn.send(message.encode())
+
+
+"""
+Recieves a new message from given socket.
+Prints debug info, then parses the message using chatlib.
+Paramaters: conn (socket object)
+Returns: cmd (str) and data (str) of the received message.
+If error occured, will return None, None
+"""
 
 
 def recv_message_and_parse(conn):
@@ -27,14 +36,12 @@ def recv_message_and_parse(conn):
         print("Problem Occurred")
 
     return cmd, msg
-	
-	
+
 
 def connect():
     """connect the client to the server
-
     Returns:
-        socket[socket]: 
+        socket[socket]:
     """
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# defining the socket
     server_socket.bind((SERVER_IP, SERVER_PORT))# setting the current ip and port
@@ -45,27 +52,34 @@ def connect():
 
 
 def error_and_exit(msg):
-    # Implement code
-    pass
+    print(msg)
+    exit()
 
 
 def login(conn):
+    print("Login...")
     username = input("Please enter username: \n")
-    # Implement code
-	
-	build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["logout_msg"],"")
-	
-	# Implement code
-	
-    pass
+    password = input("Please enter password: \n")
+    data = username + '#' + password  # used for the protocol
+
+    message = build_message('LOGIN', data)
+    print("Msg: " + message)
+    # conn.send(message.encode())
+
+    build_and_send_message(conn, PROTOCOL_CLIENT["logout_msg"], "")
+
 
 def logout(conn):
-    # Implement code
-    pass
+    print("Logout...")
+    build_and_send_message(conn, PROTOCOL_CLIENT["logout_msg"], "")
+
 
 def main():
-    # Implement code
-    pass
+    conn_sock = connect()
+    print("One Player Connected")
+    login(conn_sock)
+    logout(conn_sock)
+
 
 if __name__ == '__main__':
     main()
