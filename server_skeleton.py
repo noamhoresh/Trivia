5f32d47dec30c74fd50e0d39e047f40dd6024870
noamhoresh <59334606+noamhoresh@users.noreply.github.com>
2021-02-07 19:04:01 +0200

    Exc 6 server

--- server_skeleton.py
+++ server_skeleton.py
@@ -7,23 +7,23 @@ from chatlib_skeleton import *
 
 # GLOBALS
 users = {
-	"abc": ['123', 0],
-	"test": ["test", 0],
-	"admin": ["AaBbCcDd", 0],
-	"blabla": ["hello", 0],
-	"super": ["trooper", 0],
-	"super2": ["trooper2", 0],
-	"bilby": ["m0unt41ns", 0],
-	"trivia_king": ["KING", 0],
-	"hackerman": ["TriCeRaCop", 0],
-	"mrhemulin": ["flowerz", 0],
-	"bambababy": ["peanuts", 5, (0,2)]
-	}
+    "abc": ['123', 13],
+    "test": ["test", 0],
+    "admin": ["AaBbCcDd", 0],
+    "blabla": ["hello", 0],
+    "super": ["trooper", 0],
+    "super2": ["trooper2", 0],
+    "bilby": ["m0unt41ns", 0],
+    "trivia_king": ["KING", 0],
+    "hackerman": ["TriCeRaCop", 0],
+    "mrhemulin": ["flowerz", 0],
+    "bambababy": ["peanuts", 5, (0, 2)]
+}
 questions = {
-            "What is the capital city of USA?|Washington DC|New York|Los Angeles|Detroit|1",
-	    "Who wrote the song ""Yellow submarine""?|Elvis Presley|The Beatles|Led Zeppelin|Britney Spears|2",
-	    "How much is 1+1?|5|6|7|2|4"
-	    }
+    "What is the capital city of USA?|Washington DC|New York|Los Angeles|Detroit|1",
+    "Who wrote the song ""Yellow submarine""?|Elvis Presley|The Beatles|Led Zeppelin|Britney Spears|2",
+    "How much is 1+1?|5|6|7|2|4"
+}
 logged_users = {}  # a dictionary of client hostnames to usernames - will be used later
 
 ERROR_MSG = "Error! "
@@ -34,51 +34,53 @@ SERVER_IP = "127.0.0.1"
 # HELPER SOCKET METHODS
 
 def build_and_send_message(conn, code, msg):
-	message = build_message(code, msg)
-	conn.send(message.encode())
-	print("[SERVER] ", message)	  # Debug print
+    message = build_message(code, msg)
+    conn.send(message.encode())
+    print("[SERVER] ", message)  # Debug print
 
 
 def recv_message_and_parse(conn):
-	data = conn.recv(2048).decode()
-	print("Client Response: " + data)
-	cmd, msg = parse_message(data)
+    data = conn.recv(2048).decode()
+    print("Client Response: " + data)
+    cmd, msg = parse_message(data)
 
-	if cmd is None:
-		print("Problem Occurred")
+    if cmd is None:
+        print("Problem Occurred")
 
-	return cmd, msg
-	
+    return cmd, msg
+
 
 # Data Loaders #
 
 def load_questions():
-	"""
+    """
 	Loads questions bank from file	## FILE SUPPORT TO BE ADDED LATER
 	Recieves: -
 	Returns: questions dictionary
 	"""
-	questions = {
-				2313 : {"question":"How much is 2+2","answers":["3","4","2","1"],"correct":2},
-				4122 : {"question":"What is the capital of France?","answers":["Lion","Marseille","Paris","Montpellier"],"correct":3} 
-				}
-	
-	return questions
+    questions = {
+        2313: {"question": "How much is 2+2", "answers": ["3", "4", "2", "1"], "correct": 2},
+        4122: {"question": "What is the capital of France?", "answers": ["Lion", "Marseille", "Paris", "Montpellier"],
+               "correct": 3}
+    }
 
-def load_user_database():
-	"""
-	Loads users list from file	## FILE SUPPORT TO BE ADDED LATER
-	Recieves: -
-	Returns: user dictionary
-	"""
-	users = {
-			"test"		:	{"password":"test","score":0,"questions_asked":[]},
-			"yossi"		:	{"password":"123","score":50,"questions_asked":[]},
-			"master"	:	{"password":"master","score":200,"questions_asked":[]}
-			}
-	return users
+    return questions
 
-	
+
+# def load_user_database():
+#     """
+# 	Loads users list from file	## FILE SUPPORT TO BE ADDED LATER
+# 	Recieves: -
+# 	Returns: user dictionary
+# 	"""
+#     users = {
+#         "test"	:	{"password" :"test" ,"score" :0 ,"questions_asked" :[]},
+# 		"yossi"		:	{"password" :"123" ,"score" :50 ,"questions_asked" :[]},
+# 		"master"	:	{"password" :"master" ,"score" :200 ,"questions_asked" :[]}
+# 	}
+# 	return users
+
+
 # SOCKET CREATOR
 
 def setup_socket():
@@ -92,9 +94,9 @@ def setup_socket():
 	server_socket.listen()
 	print("Listening for connections on port %d" % SERVER_PORT)
 	return server_socket
-	
 
-		
+
+
 def send_error(conn, error_msg):
 	"""
 	Send error message with given message
@@ -102,25 +104,24 @@ def send_error(conn, error_msg):
 	Returns: None
 	"""
 	build_and_send_message(conn, PROTOCOL_SERVER["error_cmd"], error_msg)
-	
-	
 
 
-	
+
+
+
 ##### MESSAGE HANDLING
 
 
 def handle_getscore_message(conn, username):
 	"""send the client his current score
-
 	Args:
 		conn (socket)
 		username (str)
 	"""
-	build_and_send_message(conn, PROTOCOL_SERVER["score_ok_msg"], users[username][1])
-	
+	build_and_send_message(conn, PROTOCOL_SERVER["score_ok_msg"], str(users[username][1]))
 
-	
+
+
 def handle_logout_message(conn):
 	"""
 	Closes the given socket (in laster chapters, also remove user from logged_users dictioary)
@@ -128,7 +129,10 @@ def handle_logout_message(conn):
 	Returns: None
 	"""
 	global logged_users
-	logged_users.pop(conn.getpeername(),None)
+	global flag
+	flag = False
+	logged_users.pop(conn.getpeername(), None)
+	build_and_send_message(conn,"LOGOUT_OK","")
 	conn.close()
 	print("Logged Out")
 
@@ -144,22 +148,22 @@ def handle_login_message(conn, data):
 	global logged_users	 # To be used later
 	server_response = ""
 	login_details = data.split("#")
-	
+
 	if len(login_details) != 2:
 		server_response = "Invalid Data For Login"
-	
-	else: 
+
+	else:
 		client_user_name = login_details[0]
 		if client_user_name in users.keys(): # if the user name exists
 			client_password = login_details[1]
-			
-			if client_password == users[client_user_name][0]:# if the password exists
+
+			if client_password == users[client_user_name][0]  :# if the password exists
 				build_and_send_message(conn, PROTOCOL_SERVER["login_ok_msg"], "")
 				logged_users[conn.getpeername()] = client_user_name # add the user to logged users dict
 				return
-		
-		server_response = "User Name Or Password Does Not Exists"		
-	
+
+		server_response = "User Name Or Password Does Not Exists"
+
 	send_error(server_response)
 	return
 
@@ -186,7 +190,7 @@ def handle_client_message(conn, cmd, data):
 	elif cmd == "SEND_ANSWER":
 		pass
 	elif cmd == "MY_SCORE":
-		handle_getscore_message(conn,data.split("#")[0])# the user_name of the current user
+		handle_getscore_message(conn, data.split("#")[0])# the user_name of the current user
 	elif cmd == "HIGHSCORE":
 		pass
 	elif cmd == "" and data == "":
@@ -194,18 +198,20 @@ def handle_client_message(conn, cmd, data):
 	else:
 		send_error()
 
-	
 
 def main():
 	# Initializes global users and questions dicionaries using load functions, will be used later
 	global users
+	global flag
 	global questions
-	
+	flag = True
 	print("Welcome to Trivia Server!")
 	server_socket = setup_socket()
-	while True:
-		client_socket, client_address = server_socket.accept()
-		print("new connection received")
+
+	client_socket, client_address = server_socket.accept()
+	print("new connection received")
+
+	while flag:
 		cmd, data = recv_message_and_parse(client_socket)
 		handle_client_message(client_socket, cmd, data)
 
